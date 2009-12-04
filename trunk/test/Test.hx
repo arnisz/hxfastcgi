b08bfa494c5adeedf1fe1508e7f3d41d3fad19ea
gamehaxe <gamehaxe@419a14fa-e081-11de-a155-4d945b6d75ec>
2009-12-04 05:34:30 +0000

    Get FastCGI running on linux

--- trunk/test/Test.hx
+++ trunk/test/Test.hx
@@ -1,17 +1,20 @@
 class Test
 {
+   static var processed = 0;
+
    static public function main()
    {
       // Called in single threaded mode...
-      fastcgi.Request.init(":2288");
+      //fastcgi.Request.init(":2288");
+      fastcgi.Request.init("");
       // This can be called multi-threaded...
       var req = new fastcgi.Request();
-      if( req.nextRequest() )
+      while( req.nextRequest() )
       {
          req.write( "Content-type: text/html\r\n" +
             "\r\n" +
-            "<title>FastCGI Hello! (multi-threaded C, fcgiapp library)</title>" +
-            "<h1>FastCGI Hello! (multi-threaded C, fcgiapp library)</h1>" );
+            "<title>Neko FastCGI</title>" +
+            "<h1>Fast CGI</h1> Requests processed here: " + (processed++) );
 
          req.close();
       }
